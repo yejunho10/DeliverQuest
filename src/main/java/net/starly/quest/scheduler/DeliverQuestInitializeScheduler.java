9e1a6a7dbbb0c90f668f4efdd023faa9378278fb
yejunho10 <hyjcompany30@naver.com>
2023-05-28 09:51:00 +0900

    fix: Bugs

--- src/main/java/net/starly/quest/scheduler/DeliverQuestInitializeScheduler.java
+++ src/main/java/net/starly/quest/scheduler/DeliverQuestInitializeScheduler.java
@@ -4,19 +4,18 @@ import net.starly.quest.YDDailyQuestMain;
 import net.starly.quest.deliver.manager.DeliverAssignManager;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.time.LocalDateTime;
+import java.time.*;
 
 public class DeliverQuestInitializeScheduler extends BukkitRunnable {
 
     private static BukkitRunnable instance = null;
 
     public static void start() {
-        final LocalDateTime now = LocalDateTime.now();
-        final int TICK_PER_SECOND = 20;
-        final int TICK_PER_MINUTE = 60 * TICK_PER_SECOND;
-        final int TICK_PER_HOUR = 60 * TICK_PER_MINUTE;
-        final int TICK_PER_DAY = 24 * TICK_PER_HOUR;
-        final int DELAY = ((9 - now.getHour()) * TICK_PER_HOUR) + ((2 - now.getMinute()) * TICK_PER_MINUTE) + ((60 - now.getSecond()) * TICK_PER_SECOND);
+        LocalTime now = LocalTime.now();
+        long passedSecondOfDay = now.toSecondOfDay();
+        final long SECONDS_OF_DAY = 24 * 60 * 60;
+        final long TICK_PER_DAY = SECONDS_OF_DAY * 20;
+        final long DELAY = (SECONDS_OF_DAY - passedSecondOfDay) * 20L;
 
         // ※ 24시 0분 0초에 실행할 시, 그 즉시는 실행안됨 ※
         instance = new DeliverQuestInitializeScheduler();
